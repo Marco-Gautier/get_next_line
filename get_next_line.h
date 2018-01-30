d4af4b36abb2df2fdaa7eafd08f14f303ef7ccfc
marco.gautier <marco.gautier@localhost.localdomain>
2018-01-30 16:10:58 +0100

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2017
+** CPE_getnextline_2017
+** File description:
+** __DESCRIPTION__
+*/
+
+#pragma once
+
+char *get_next_line(int fd);
+
+#ifndef READ_SIZE
+#define READ_SIZE 200
+#endif
